9e38a37a5bd13f377f3366797f2c02926f934ec2
Jermaine Simon Davies <jermainedavies@outlook.com>
2020-01-25 13:46:51 +0000

    Add files via upload

---
+++ Invasion_of_Oz.py
@@ -0,0 +1,35 @@
+#remake of space invaders written in Python by Jermaine Simon Davies
+
+import turtle
+import time
+
+#windowsize
+
+wn = turtle.Screen()
+wn.title("Pink Pong by @jermainedavies")
+wn.bgcolor("black")
+wn.setup(width=450, height=600)
+wn.tracer(0)
+
+
+#witch size = 10*10
+#girl size = 10*10
+
+#witch movement
+#every second move witch by 5
+#witchx_cor = -190
+#witchy_cor = 290
+
+#if seconds ==+ 1:
+#    witchx_cor += 5
+
+#if witchy_cor == 190:
+#    withchy_cor -= 5
+
+
+#human movement
+#girlx_cor = 0
+#girly_cor = -290
+
+
+#water movement
